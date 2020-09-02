532c6f2a6400ff7ae1b14b8cd5b0b7bdcc8b5cc9
kermit <70634364+zerotrzy@users.noreply.github.com>
2020-09-02 17:28:29 +0200

    Poprawka wyglądu
    Teraz zawęziłem pliki do pakowania tylko do tego co trzeba.
    Poprawiłem lekko wygląd i tło głównego okna.
    
    Może się komus przyda, kto będzie zaczynał z electronem

--- main.js
+++ main.js
@@ -2,42 +2,47 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+function createWindow ()
+{
+ // Główne okno przegladarki
+ const oknoProgramu = new BrowserWindow({
+    width: 800, height: 600,
+    backgroundColor: '#8194a3',
+    textAreasAreResizable: false, // nie da się zwiększyć tekstu
+    resizable: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
+ })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+ // możemy wywalić domyslne menu
+ oknoProgramu.removeMenu()
+ // wczytujemy plik html
+ oknoProgramu.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+ // oknoProgramu.webContents.openDevTools()
+ // oknoProgramu.isResizable(true); // jeśli ma być zwiększane okno
+ oknoProgramu.focus(); // okno ustawione do działania
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
+// Jak już wszystko ustawiliśmy to możemy uruchomić następne
+// funkcje. Czasem tylko tu da się to uruchomić.
+app.whenReady().then(() =>
+{
+ createWindow()
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+ app.on('activate', function ()
+  {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+// Zakończ program za pomocą Ctrl+Q
+app.on('window-all-closed', function ()
+{
+ if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Tu da się dodać kolejne pliki dla głównego procesu programu
